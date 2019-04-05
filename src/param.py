f976841d98cd8da0cd603fd3ecb328f709fb7a63
756462516@qq.com <756462516@qq.com>
2019-04-05 17:06:48 +0800

    update

--- src/param.py
+++ src/param.py
@@ -4,8 +4,10 @@ data_path = '../data'
 
 cv_train_num = 100000  # 用于交叉验证
 
-train_num = 120000
-test_num = 90000
+# train_num = 120000
+train_num = 40000
+# test_num = 90000
+test_num = 30000
 
 w2v_dim = 300
 
